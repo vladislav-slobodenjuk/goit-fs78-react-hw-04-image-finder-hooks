eb01de6ae180edf8bc28b729fda53fdb5385c1e5
vladislav-slobodenjuk <vladjun@gmail.com>
2023-07-11 00:56:27 +0300

    refactor App`s state to hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { PER_PAGE, getImages } from 'services/api';
 
 import { Container } from './App.styled';
@@ -9,18 +9,26 @@ import { Modal } from 'components/Modal/Modal';
 import { Loader } from 'components/Loader/Loader';
 import { Notification } from '../Notification/Notification';
 
-export class App extends Component {
-  state = {
-    search: '',
-    page: 1,
-    totalPages: 1,
-    images: [],
-    modal: { isOpen: false, img: null },
-    error: null,
-    isLoading: false,
-  };
+export const App = () => {
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [images, setImages] = useState([]);
+  const [modal, setModal] = useState({ isOpen: false, img: null });
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  handleSubmit = async e => {
+  // state = {
+  //   search: '',
+  //   page: 1,
+  //   totalPages: 1,
+  //   images: [],
+  //   modal: { isOpen: false, img: null },
+  //   error: null,
+  //   isLoading: false,
+  // };
+
+  const handleSubmit = async e => {
     e.preventDefault();
     const querry = e.target.search.value.trim();
 
@@ -30,95 +38,90 @@ export class App extends Component {
     this.setState({ search: querry, page: 1, totalPages: 1 });
   };
 
-  openModal = img => {
+  const openModal = img => {
     this.setState({ modal: { isOpen: true, img } });
   };
 
-  closeModal = () => {
+  const closeModal = () => {
     this.setState({ modal: { isOpen: false, img: null } });
   };
 
-  loadMore = () => {
+  const loadMore = () => {
     this.setState(prev => ({ page: prev.page + 1 }));
   };
 
-  async componentDidUpdate(_, prevState) {
-    const { search, page, images } = this.state;
+  // async componentDidUpdate(_, prevState) {
+  //   const { search, page, images } = this.state;
 
-    if (prevState.images < images && page !== 1) {
-      window.scrollBy({ top: 520, behavior: 'smooth' });
-    }
+  //   if (prevState.images < images && page !== 1) {
+  //     window.scrollBy({ top: 520, behavior: 'smooth' });
+  //   }
 
-    if (prevState.search !== search) {
-      try {
-        this.setState({ isLoading: true });
+  //   if (prevState.search !== search) {
+  //     try {
+  //       this.setState({ isLoading: true });
 
-        const { hits, totalHits } = await getImages(search, page);
-        this.setState({
-          images: hits,
-          totalPages: Math.ceil(totalHits / PER_PAGE),
-        });
-        //
-      } catch (error) {
-        this.setState({ error });
-        console.log(error.message);
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
+  //       const { hits, totalHits } = await getImages(search, page);
+  //       this.setState({
+  //         images: hits,
+  //         totalPages: Math.ceil(totalHits / PER_PAGE),
+  //       });
+  //       //
+  //     } catch (error) {
+  //       this.setState({ error });
+  //       console.log(error.message);
+  //     } finally {
+  //       this.setState({ isLoading: false });
+  //     }
+  //   }
 
-    if (prevState.page !== page && page !== 1) {
-      try {
-        this.setState({ isLoading: true });
+  //   if (prevState.page !== page && page !== 1) {
+  //     try {
+  //       this.setState({ isLoading: true });
 
-        const { hits } = await getImages(search, page);
-        this.setState({ images: [...prevState.images, ...hits] });
-        //
-      } catch (error) {
-        this.setState({ error });
-        console.log(error.message);
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-  }
+  //       const { hits } = await getImages(search, page);
+  //       this.setState({ images: [...prevState.images, ...hits] });
+  //       //
+  //     } catch (error) {
+  //       this.setState({ error });
+  //       console.log(error.message);
+  //     } finally {
+  //       this.setState({ isLoading: false });
+  //     }
+  //   }
+  // }
 
-  render() {
-    const { search, images, modal, isLoading, page, totalPages, error } =
-      this.state;
-    // console.log('render');
+  // const { search, images, modal, isLoading, page, totalPages, error } =
+  //   this.state;
 
-    return (
-      <Container>
-        <Searchbar handleSubmit={this.handleSubmit} />
-        <ImageGallery images={images} onImgClick={this.openModal} />
+  return (
+    <Container>
+      <Searchbar handleSubmit={handleSubmit} />
+      <ImageGallery images={images} onImgClick={openModal} />
 
-        {images.length > 0 && page < totalPages && (
-          <Button text="Load more" onButtonClick={this.loadMore} />
-        )}
+      {images.length > 0 && page < totalPages && (
+        <Button text="Load more" onButtonClick={loadMore} />
+      )}
 
-        {isLoading && <Loader />}
+      {isLoading && <Loader />}
 
-        {modal.isOpen && (
-          <Modal img={modal.img} onCloseModal={this.closeModal} />
-        )}
+      {modal.isOpen && <Modal img={modal.img} onCloseModal={closeModal} />}
 
-        {search.length === 0 && (
-          <Notification text="Come on, look for something" />
-        )}
+      {search.length === 0 && (
+        <Notification text="Come on, look for something" />
+      )}
 
-        {search.length !== 0 && images.length === 0 && !isLoading && !error && (
-          <Notification text="Nothing found 😔" />
-        )}
+      {search.length !== 0 && images.length === 0 && !isLoading && !error && (
+        <Notification text="Nothing found 😔" />
+      )}
 
-        {error && (
-          <Notification text="Oops, something went wrong 😬 Try again later" />
-        )}
+      {error && (
+        <Notification text="Oops, something went wrong 😬 Try again later" />
+      )}
 
-        {page === totalPages && page !== 1 && (
-          <Notification text="That's all for now" />
-        )}
-      </Container>
-    );
-  }
-}
+      {page === totalPages && page !== 1 && (
+        <Notification text="That's all for now" />
+      )}
+    </Container>
+  );
+};
